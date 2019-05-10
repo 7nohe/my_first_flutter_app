7c2017daf239ba1630104de743966e5333a0e42c
7nohe <7nohe.urata@gmail.com>
2019-05-10 14:04:21 +0900

    :sparkles: add firestore package

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
+
+final dummySnapshot = [
+  {
+    'name': '糸島ラーメン',
+    'category': '0',
+    'breakfast': false,
+    'lunch': true,
+    'dinner': true,
+    'visited': true
+  },
+  {
+    'name': '侑久上海',
+    'category': '1',
+    'breakfast': false,
+    'lunch': true,
+    'dinner': true,
+    'visited': true
+  },
+  {
+    'name': 'UNO',
+    'category': '2',
+    'breakfast': false,
+    'lunch': true,
+    'dinner': true,
+    'visited': true
+  },
+  {
+    'name': 'USHIO',
+    'category': '3',
+    'breakfast': false,
+    'lunch': false,
+    'dinner': true,
+    'visited': true
+  },
+];
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Eatock',
       theme: ThemeData(
         primaryColor: Colors.white,
-      ),   
-      home: RandomWords(),
+      ),
+      home: HomePage(),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
   final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('EaTock'),
         actions: <Widget>[
           // Add 3 lines from here...
           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
         ],
       ),
-      body: _buildSuggestions(),
+      body: _buildBody(context),
     );
   }
 
@@ -65,47 +105,61 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
+  Widget _buildBody(BuildContext context) {
+    // TODO: get actual snapshot from Cloud Firestore
+    return _buildList(context, dummySnapshot);
   }
 
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
+  Widget _buildList(BuildContext context, List<Map> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.all(16.0),
+      children: snapshot.map((data) => _buildRow(context, data)).toList(),
+    );
+  }
+
+  Widget _buildRow(BuildContext context, Map data) {
+    final _stockItem = StockItem.fromMap(data);
+
     return ListTile(
       title: Text(
-        pair.asPascalCase,
+        _stockItem.name,
         style: _biggerFont,
       ),
       trailing: Icon(
         // Add the lines from here...
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
+        // alreadySaved ? Icons.favorite : Icons.favorite_border,
+        Icons.favorite_border,
+        // color: alreadySaved ? Colors.red : null,
+        color: Colors.red
       ),
       onTap: () {
         // Add 9 lines from here...
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
+        // setState(() {
+        //   if (alreadySaved) {
+        //     _saved.remove(pair);
+        //   } else {
+        //     _saved.add(pair);
+        //   }
+        // });
       },
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class StockItem {
+  final String name;
+  final bool visited;
+  final DocumentReference reference;
+
+  StockItem.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['visited'] != null),
+        name = map['name'],
+        visited = map['visited'];
+
+  StockItem.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
   @override
-  RandomWordsState createState() => RandomWordsState();
+  String toString() => 'StockItem<$name:$visited>';
 }
